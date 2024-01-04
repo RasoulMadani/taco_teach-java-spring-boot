404fc21c4fc3d562698615879acf3347bcdbae3d
Rasoulmadani <ali.rahbary@chmail.ir>
2024-01-05 01:59:42 +0330

    initial spring boot

---
+++ src/main/java/ir/jahangiri/taco_teach/TacoTeachApplication.java
@@ -0,0 +1,13 @@
+package ir.jahangiri.taco_teach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoTeachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoTeachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/jahangiri/taco_teach/TacoTeachApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.jahangiri.taco_teach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoTeachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
